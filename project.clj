377bcedd3ddcb91271b41b65d5e598deb15bdc9a
Terpo <nicolas.terpolilli@gmail.com>
2014-10-14 11:50:32 +0200

    dependencies DONE + first really light handler

--- project.clj
+++ project.clj
@@ -4,7 +4,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.9"]
-                 [ring-middleware-format "0.2.2"]]
+                 [ring-middleware-format "0.2.2"]
+                 [ring/ring-json "0.3.1"]
+                 [com.taoensso/timbre "3.1.6"]
+                 [com.novemberain/monger "2.0.0"]
+                 [com.novemberain/validateur "1.7.0"]
+                 [slingshot "0.10.3"]
+                 [clj-time "0.6.0"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler hunter-api.handler/app}
   :profiles
--- src/hunter_api/handler.clj
+++ src/hunter_api/handler.clj
@@ -1,12 +1,19 @@
 (ns hunter-api.handler
+  (:use ring.util.response)
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.middleware.format-response :refer [wrap-restful-response]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/resources "/")
+(defroutes api-routes
+  "Main client API route definitions"
+  (context "/api" []
+           (OPTIONS "/" []
+                    (-> (response {:version "0.1.0-SNAPSHOT"})
+                        (header "Allow" "OPTIONS"))))
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (->
+   (handler/api api-routes)
+   (wrap-restful-response)))
