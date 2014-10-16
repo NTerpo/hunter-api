fdb148aea80788f052869da2acac42aa11c21a37
Terpo <nicolas.terpolilli@gmail.com>
2014-10-16 17:27:46 +0200

    create MongoDB Connections & a basic create-dataset function + tests

--- src/hunter_api/data.clj
+++ src/hunter_api/data.clj
@@ -15,11 +15,12 @@
 (def mongo-options
   {:host "localhost"
    :port 27017
-   :db "datasets"
+   :db "hunter-datasets"
    :datasets-collection "ds"})
 
-(def conn (connect mongo-options))
-(get-db conn (mongo-options :db)) ;; to be tested!
+(comment 
+  (def conn (connect mongo-options))
+  (get-db conn (mongo-options :db)))
 
 ;;; Utility Functions
 
@@ -37,6 +38,57 @@
 
 ;;; Validation Functions
 
+(defmulti validate* (fn [val val-type] val-type))
+
+(defmethod validate* ::ObjectID
+  [id _]
+  (if-not (and
+           (not (nil? id))
+           (string? id)
+           (re-matches #"[0-9a-f]{24}" id))
+    (throw+ {:type ::invalid} "Invalid ID")))
+
+(defmethod validate* ::Dataset
+  [dataset _]
+  (if-not (valid? (validation-set
+                   (presence-of :_id)
+                   (presence-of :created)
+                   (presence-of :modified)
+                   (presence-of :date)) dataset)
+    (throw+ {:type ::invalid} "Invalid Dataset")))
+
+(defn validate
+  "Execute a sequence of validation tests"
+  [& tests]
+  (doseq [test tests] (apply validate* test)))
 
 ;;; Database Access Functions
 
+(defn create-dataset
+  "Insert a dataset into the database"
+  ([ds]
+      (let [new-ds (-> ds
+                       with-oid
+                       modify-now
+                       create-now)
+            conn (connect mongo-options)
+            db (get-db conn (mongo-options :db))]
+        (validate [new-ds ::Dataset])
+        (if (ok? (collection/insert db
+                                    (mongo-options :datasets-collection)
+                                    new-ds))
+          new-ds
+          (throw+ {:type ::failed} "Create Failed"))))
+  ([ds db]
+     (let [new-ds (-> ds
+                       with-oid
+                       modify-now
+                       create-now)
+            conn (connect mongo-options)
+            db (get-db conn db)]
+        (validate [new-ds ::Dataset])
+        (if (ok? (collection/insert db
+                                    (mongo-options :datasets-collection)
+                                    new-ds))
+          new-ds
+          (throw+ {:type ::failed} "Create Failed")))))
---
+++ test/hunter_api/test/data.clj
@@ -0,0 +1,47 @@
+(ns hunter-api.test.data
+  (:require [clojure.test :refer :all]
+            [clj-time.core :as time]
+            [hunter-api.data :refer :all]
+            [monger.core :refer [connect get-db]]
+            [slingshot.test :refer :all]))
+
+(defn my-test-fixture
+  [f]
+  (def connection (connect {:host "localhost"
+                            :port 27017}))
+  (get-db connection "hunter-datasets-test")
+  (f))
+
+(use-fixtures :once my-test-fixture)
+
+(deftest test-validation
+  (testing "valid dataset ID"
+    (is (nil? (validate ["543e62ab40694721af85ae5f" :hunter-api.data/ObjectID]))))
+  (testing "invalid dataset ID"
+    (is (thrown+? [:type :hunter-api.data/invalid] (validate ["123456789" :hunter-api.data/ObjectID]))))
+  (testing "valid dataset"
+    (is (nil? (validate [(-> {:foo "Bar"
+                              :date 2014}
+                             with-oid
+                             modify-now
+                             create-now) :hunter-api.data/Dataset]))))
+  (testing "invalid dataset"
+    (is (thrown+? [:type :hunter-api.data/invalid] (validate [{:foo "Bar"} :hunter-api.data/Dataset])))))
+
+(deftest test-create-dataset
+  (testing "create valid dataset"
+    (let [ds {:foo "Bar"
+              :date 1990
+              :zone "France"
+              :swag 1}
+          created-ds (create-dataset ds "hunter-datasets-test")]
+      (is (map? created-ds))
+      (is (contains? created-ds :_id))
+      (is (contains? created-ds :foo))
+      (is (contains? created-ds :date))
+      (is (contains? created-ds :zone))
+      (is (contains? created-ds :swag))
+      (is (contains? created-ds :created))
+      (is (contains? created-ds :modified))))
+  (testing "create invalid dataset"
+    (is (thrown+? [:type :hunter-api.data/invalid] (create-dataset {} "hunter-datasets-test")))))
