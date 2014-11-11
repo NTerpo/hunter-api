4a42ab85c68fb14fcfa9f19d1390183f79f1b2c0
Terpo <nicolas.terpolilli@gmail.com>
2014-11-11 17:23:27 +0100

    refactoring test data.clj

--- test/hunter_api/test/data.clj
+++ test/hunter_api/test/data.clj
@@ -3,7 +3,7 @@
             [clj-time.core :as time]
             [clj-time.format :as f]
             [hunter-api.data :refer :all]
-            [hunter-api.test.ds :refer [valid-dataset ds1 ds2]]
+            [hunter-api.test.ds :refer [valid-dataset ds1 ds2 ds3]]
             [monger.core :refer [connect get-db]]
             [slingshot.test :refer :all]))
 
@@ -11,9 +11,11 @@
 
 (deftest test-date->valid-date
   (testing "YYYY-MM-dd"
-    (is (= "1937-07-18" (f/unparse multi-parser (date->valid-date "1937-07-18")))))
+    (is (= "1937-07-18"
+           (f/unparse multi-parser (date->valid-date "1937-07-18")))))
   (testing "YYYY/MM/dd"
-    (is (= "1937-07-18" (f/unparse multi-parser (date->valid-date "1937/07/18")))))
+    (is (= "1937-07-18"
+           (f/unparse multi-parser (date->valid-date "1937/07/18")))))
   (testing "invalid date format"
     (is (thrown? IllegalArgumentException
                  (f/unparse multi-parser (date->valid-date "19370718"))))))
@@ -21,8 +23,10 @@
 (deftest test-normalize-dates
   (testing "with :created and :updated"
     (let [ds (normalize-dates valid-dataset)]
-      (is (= (date->valid-date "0666-01-01") (ds :created)))
-      (is (= (date->valid-date "0666-01-02") (ds :updated)))))
+      (is (= (date->valid-date "0666-01-01")
+             (ds :created)))
+      (is (= (date->valid-date "0666-01-02")
+             (ds :updated)))))
   (testing "without :created and :updated"
     (let [ds (normalize-dates {})]
       (is (empty? ds)))))
@@ -31,14 +35,16 @@
   (testing "valid dataset ID"
     (is (nil? (validate ["543e62ab40694721af85ae5f" :hunter-api.data/ObjectID]))))
   (testing "invalid dataset ID"
-    (is (thrown+? [:type :hunter-api.data/invalid] (validate ["123456789" :hunter-api.data/ObjectID]))))
+    (is (thrown+? [:type :hunter-api.data/invalid]
+                  (validate ["123456789" :hunter-api.data/ObjectID]))))
   (testing "valid dataset"
     (is (nil? (validate [(-> valid-dataset
                              with-oid
                              modify-now
                              create-now) :hunter-api.data/Dataset]))))
   (testing "invalid dataset"
-    (is (thrown+? [:type :hunter-api.data/invalid] (validate [{:foo "Bar"} :hunter-api.data/Dataset])))))
+    (is (thrown+? [:type :hunter-api.data/invalid]
+                  (validate [{:foo "Bar"} :hunter-api.data/Dataset])))))
 
 (deftest test-create-dataset
   (testing "create valid dataset"
@@ -46,7 +52,8 @@
       (is (map? created-ds))
       (is (nil? (validate [created-ds :hunter-api.data/Dataset])))))
   (testing "create invalid dataset"
-    (is (thrown+? [:type :hunter-api.data/invalid] (create-dataset {} api-db-test)))))
+    (is (thrown+? [:type :hunter-api.data/invalid]
+                  (create-dataset {} api-db-test)))))
 
 (deftest test-get-dataset
   (testing "get valid dataset"
@@ -56,7 +63,8 @@
       (is (nil? (validate [ds :hunter-api.data/Dataset])))
       (is (= (created-ds ds)))))
   (testing "get dataset with invalid id"
-    (is (thrown+? [:type :hunter-api.data/invalid] (get-dataset "666" api-db-test))))
+    (is (thrown+? [:type :hunter-api.data/invalid]
+                  (get-dataset "666" api-db-test))))
   (testing "get non-existent dataset"
     (is (thrown+? [:type :hunter-api.data/not-found] (get-dataset "543e62ab40694721af85ae5f" api-db-test)))))
 
@@ -66,25 +74,25 @@
           deleted (delete-dataset (.toString (created-ds :_id)) api-db-test)]
       (is (not (nil? deleted)))))
   (testing "delete with invalid id"
-    (is (thrown+? [:type :hunter-api.data/invalid] (delete-dataset "666" api-db-test)))))
+    (is (thrown+? [:type :hunter-api.data/invalid]
+                  (delete-dataset "666" api-db-test)))))
 
 (deftest test-find-dataset
   (testing "finding a dataset"
     (let [ds1 (create-dataset ds1  api-db-test)
-          ds2 (create-dataset  ds2 api-db-test)
-          found (last (find-dataset {:temporal "0667"} api-db-test))
-          found-2 (last (find-dataset {:publisher "foo"} api-db-test))]
-      (is (= (found :title) (ds2 :title)))
-      (is (= (found :description) (ds2 :description)))
-      (is (= (found :publisher) (ds2 :publisher)))
-      (is (= (found :temporal) (ds2 :temporal)))
-      (is (= (found :spatial) (ds2 :spatial)))
-      (is (= (found :created) (ds2 :created)))
-      (is (= (found :updated) (ds2 :updated)))
-      (is (= (found :uri) (ds2 :uri)))
-      (is (= (found :tags) (ds2 :tags)))
-      (is (= (found-2 :title) (ds1 :title)))))
+          ds2 (create-dataset ds2 api-db-test)
+          ds3 (create-dataset ds3 api-db-test)
+          found (last (find-dataset {:title "test2"} api-db-test))
+          found-2 (last (find-dataset {:spatial "baz"} api-db-test))
+          found-3 (last (find-dataset {:publisher "foo"} api-db-test))]
+      (is (= (ds2 :title)
+             (found :title)))
+      (is (= (ds2 :title)
+             (found-2 :title)))
+      (is (= (ds1 :title)
+             (found-3 :title)))))
   (testing "not found dataset"
     (is (thrown+? [:type :hunter-api.data/not-found]
                   (find-dataset
-                   {:temporal "666"} api-db-test)))))
+                   {:temporal "666"}
+                   api-db-test)))))
--- test/hunter_api/test/ds.clj
+++ test/hunter_api/test/ds.clj
@@ -18,14 +18,27 @@
           :created "0666-01-01"
           :updated "0666-01-12"
           :uri "http://www.foo.bar"
-          :tags ["foo" "bar"]})
+          :tags ["foo" "bar"]
+          :huntscore 25})
 
 (def ds2 {:title "test2"
           :description "test2"
           :publisher "foo"
           :temporal "0667"
-          :spatial "Bar"
+          :spatial "baz"
           :created "0666-01-03"
           :updated "0666-01-04"
           :uri "http://www.foo.bar"
-          :tags ["foo" "bar"]})
+          :tags ["foo" "bar"]
+          :huntscore 25})
+
+(def ds3 {:title "test2"
+          :description "test2"
+          :publisher "foo"
+          :temporal "0667"
+          :spatial "baz"
+          :created "0666-01-03"
+          :updated "0666-01-04"
+          :uri "http://www.foo.bar"
+          :tags ["foo" "bar"]
+          :huntscore 2})
