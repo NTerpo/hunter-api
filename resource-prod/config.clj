66de9f0b7563347b8b83d5032f26224cfbe12499
Terpo <nicolas.terpolilli@gmail.com>
2014-11-14 22:36:59 +0100

    update db name

--- resource-prod/config.clj
+++ resource-prod/config.clj
@@ -5,4 +5,4 @@
   (let [{:keys [conn db]} (connect-via-uri "mongodb://terpo:Hunter666@dogen.mongohq.com:10036/app31566584")]
     {:conn conn
      :db db
-     :db-name "hunter-datasets"}))
+     :db-name "app31566584"}))
