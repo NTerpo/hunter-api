87631f24e671d43ec044cf1aa8d495d559222c9c
Terpo <nicolas.terpolilli@gmail.com>
2014-11-14 14:16:31 +0100

    update :prod mode config

--- resource-prod/config.clj
+++ resource-prod/config.clj
@@ -3,7 +3,7 @@
 
 (def config
   (let [uri (System/genenv "MONGOHQ_URL")
-        {:keys [conn db]} (connect-via-uri uri)]
+        {:keys [conn db]} (connect-via-uri "mongodb://terpo:Hunter666@dogen.mongohq.com:10036/app31566584")]
     {:conn conn
      :db db
      :db-name "hunter-datasets"}))
