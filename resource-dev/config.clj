833505dfcc00ec752c5d090ca829ec70844b6f69
Terpo <nicolas.terpolilli@gmail.com>
2014-12-10 14:59:21 +0100

    cleaning

--- resource-dev/config.clj
+++
@@ -1,7 +0,0 @@
-(ns hunter-api.config
-  (:require [monger.core :refer [connect get-db]]))
-
-(def config {:conn (connect {:host "localhost" :port 27017})
-             :db (get-db (connect {:host "localhost" :port 27017})
-                         "hunter-datasets")
-             :db-name "hunter-datasets"})
--- resource-prod/config.clj
+++
@@ -1,8 +0,0 @@
-(ns hunter-api.config
-  (:require [monger.core :refer [connect-via-uri]]))
-
-(def config
-  (let [{:keys [conn db]} (connect-via-uri "mongodb://terpo:Hunter666@dogen.mongohq.com:10036/app31566584")]
-    {:conn conn
-     :db db
-     :db-name "app31566584"}))
